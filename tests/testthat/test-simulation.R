35f998d61c8e8aed918a9076ae5cb6b155200003
Eliot McIntire <eliotmcintire@gmail.com>
2018-02-16 12:25:19 -0800

    messages -- it was a space!

--- tests/testthat/test-simulation.R
+++ tests/testthat/test-simulation.R
@@ -434,14 +434,24 @@ test_that("conflicting function types", {
                    "defineParameter: 'plotInterval' is not of specified type 'numeric'",
                    "defineParameter: 'saveInitialTime' is not of specified type 'numeric'",
                    "defineParameter: 'saveInterval' is not of specified type 'numeric'",
-                   "child4: module code: Init: local variable.*qwerqwer.*assigned but may not be used ",
-                   "Running inputObjects for child4", "child4: module code: Init: local variable.*poiuoiu.*assigned but may not be used ",
+                   "child4: module code: Init: local variable.*qwerqwer.*assigned but may not be used",
+                   "Running inputObjects for child4", "child4: module code: Init: local variable.*poiuoiu.*assigned but may not be used",
                    "child4: outputObjects: g, g1 are assigned to sim inside Init, but are not declared in outputObjects",
                    "child4: inputObjects: b, d, f, hi, d1, test are used from sim inside Init, but are not declared in inputObjects"
   )
 
   mm <- cleanMessage(mm)
   expect_true(all(unlist(lapply(fullMessage, function(x) any(grepl(mm, pattern = x))))))
+  # cat(paste("################################################"), file = tempfile(), append = FALSE)
+  # for (x in seq(fullMessage)) {
+  #   lineNum <- "444"
+  #   theGrepEach <- grepl(mm, pattern = fullMessage[x])
+  #   theGrep <- any(theGrepEach)
+  #   if (!theGrep) {
+  #     cat(paste("\nline ", lineNum, theGrep, fullMessage[x], "\n              ", paste(mm, collapse = "\n               "), collapse = ""), file = tempfile(), append = TRUE)
+  #   }
+  #   expect_true(theGrep)
+  # }
 
   cat(xxx[1:lineWithInit], "
       sim$child4 <- 1
@@ -545,10 +555,10 @@ test_that("conflicting function types", {
   fullMessage <- c("Running inputObjects for child4", "child4: module code: co2, co3 are declared in outputObjects, but are not assigned in the module",
                    "child4: module code: ei2, ei3, ei4 are declared in inputObjects, but no default are provided in inputObjects",
                    "child4: module code: ei3 is declared in inputObjects, but is not used in the module",
-                   "child4: module code: inputObjects: local variable.*a.*assigned but may not be used ",
-                   "child4: module code: inputObjects: local variable.*fff.*assigned but may not be used ",
-                   "child4: module code: Init: local variable.*a.*assigned but may not be used ",
-                   "child4: module code: Init: local variable.*fff.*assigned but may not be used ",
+                   "child4: module code: inputObjects: local variable.*a.*assigned but may not be used",
+                   "child4: module code: inputObjects: local variable.*fff.*assigned but may not be used",
+                   "child4: module code: Init: local variable.*a.*assigned but may not be used",
+                   "child4: module code: Init: local variable.*fff.*assigned but may not be used",
                    "child4: outputObjects: g, aaa are assigned to sim inside Init, but are not declared in outputObjects",
                    "child4: inputObjects: g, co1 are assigned to sim inside inputObjects, but are not declared in inputObjects",
                    "child4: inputObjects: b, aaa are used from sim inside Init, but are not declared in inputObjects",
@@ -558,6 +568,16 @@ test_that("conflicting function types", {
   mm <- capture_messages(simInit(paths = list(modulePath = tmpdir), modules = m))
   mm <- cleanMessage(mm)
   expect_true(all(unlist(lapply(fullMessage, function(x) any(grepl(mm, pattern = x))))))
+  # for (x in seq(fullMessage)) {
+  #   lineNum <- "566"
+  #   theGrepEach <- grepl(mm, pattern = fullMessage[x])
+  #   theGrep <- any(theGrepEach)
+  #   if (!theGrep) {
+  #     cat(paste("\nline ", lineNum, theGrep, fullMessage[x], "\n              ", paste(mm, collapse = "\n               "), collapse = ""), file = tempfile(), append = TRUE)
+  #   }
+  #   expect_true(theGrep)
+  # }
+
 
 })
 
@@ -708,10 +728,10 @@ test_that("messaging with multiple modules", {
                    "Running inputObjects for test", "test: module code: co2, co3 are declared in outputObjects, but are not assigned in the module",
                    "test: module code: ei2, ei3, ei4 are declared in inputObjects, but no default are provided in inputObjects",
                    "test: module code: ei3 is declared in inputObjects, but is not used in the module",
-                   "test: module code: inputObjects: local variable.*a.*assigned but may not be used ",
-                   "test: module code: inputObjects: local variable.*fff.*assigned but may not be used ",
-                   "test: module code: Init: local variable.*a.*assigned but may not be used ",
-                   "test: module code: Init: local variable.*fff.*assigned but may not be used ",
+                   "test: module code: inputObjects: local variable.*a.*assigned but may not be used",
+                   "test: module code: inputObjects: local variable.*fff.*assigned but may not be used",
+                   "test: module code: Init: local variable.*a.*assigned but may not be used",
+                   "test: module code: Init: local variable.*fff.*assigned but may not be used",
                    "test: outputObjects: g, aaa are assigned to sim inside Init, but are not declared in outputObjects",
                    "test: inputObjects: g, co1 are assigned to sim inside inputObjects, but are not declared in inputObjects",
                    "test: inputObjects: b, aaa are used from sim inside Init, but are not declared in inputObjects",
@@ -720,8 +740,8 @@ test_that("messaging with multiple modules", {
                    "Running inputObjects for test2", "test2: module code: co1, co4 are declared in outputObjects, but are not assigned in the module",
                    "test2: module code: ei1, ei4 are declared in inputObjects, but no default are provided in inputObjects",
                    "test2: module code: ei1 is declared in inputObjects, but is not used in the module",
-                   "test2: module code: inputObjects: local variable.*a.*assigned but may not be used ",
-                   "test2: module code: Init: local variable.*a.*assigned but may not be used ",
+                   "test2: module code: inputObjects: local variable.*a.*assigned but may not be used",
+                   "test2: module code: Init: local variable.*a.*assigned but may not be used",
                    "test2: inputObjects: co1 is assigned to sim inside inputObjects, but is not declared in inputObjects",
                    "test2: inputObjects: b is used from sim inside Init, but is not declared in inputObjects",
                    "test2: inputObjects: b is used from sim inside inputObjects, but is not declared in inputObjects",
@@ -740,6 +760,17 @@ test_that("messaging with multiple modules", {
   mm1 <- cleanMessage(mm1)
   expect_true(all(unlist(lapply(fullMessage,
                                 function(x) any(grepl(mm1, pattern = x))))))
+  mm <- capture_messages(simInit(paths = list(modulePath = tmpdir), modules = as.list(m)))
+  mm <- cleanMessage(mm)
+  # for (x in seq(fullMessage)) {
+  #   lineNum <- "757"
+  #   theGrepEach <- grepl(mm, pattern = fullMessage[x])
+  #   theGrep <- any(theGrepEach)
+  #   if (!theGrep) {
+  #     cat(paste("\nline ", lineNum, theGrep, fullMessage[x], "\n              ", paste(mm, collapse = "\n               "), collapse = ""), file = tempfile(), append = TRUE)
+  #   }
+  #   expect_true(theGrep)
+  # }
 
 })
 
