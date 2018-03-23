5492828525592a64e2225cbd3c1e33a9e370a30b
Eliot McIntire <eliotmcintire@gmail.com>
2018-03-22 17:18:23 -0700

    objectSize -- must match reproducible::objectSize

--- R/cache.R
+++ R/cache.R
@@ -507,8 +507,8 @@ if (!exists("objectSize")) {
 #' a <- simInit(objects = list(d = 1:10, b = 2:20))
 #' objectSize(a)
 #' object.size(a)
-objectSize.simList <- function(x) {
-  aa <- objectSize(x@.envir)
+objectSize.simList <- function(x, quick = getOption("reproducible.quick", FALSE)) {
+  aa <- objectSize(x@.envir, quick = quick)
   bb <- as(x, "simList_")
   bb@.list <- list()
   bbOs <- list(simList = object.size(bb))
