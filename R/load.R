ee7f7f6be7fcf53496598d5e4d81f22912ad8b5b
Eliot McIntire <eliotmcintire@gmail.com>
2019-01-22 13:46:39 -0800

    test-load caught -- bugfixes in loadFiles

--- R/load.R
+++ R/load.R
@@ -124,11 +124,12 @@ setMethod(
     if (NROW(inputs(sim)) != 0) {
       inputs(sim) <- .fillInputRows(inputs(sim), start(sim))
       filelist <- inputs(sim) # does not create a copy - because data.table ... this is a pointer
-      doFilesExist <- file.exists(filelist$file)
+      nonNAFileList <- filelist[!is.na(filelist$file),]
+      doFilesExist <- file.exists(nonNAFileList$file)
       if (any(!doFilesExist))
         stop("These files in 'inputs' don't exist; please put them in the right place, ",
              "or change `inputs`:\n    ",
-             paste0(filelist$file[!doFilesExist], collapse = "\n    "))
+             paste0(nonNAFileList$file[!doFilesExist], collapse = "\n    "))
 
       curTime <- time(sim, timeunit(sim))
       arguments <- inputArgs(sim)
