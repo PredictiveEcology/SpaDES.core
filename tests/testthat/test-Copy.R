2c0143ab2e06ba024b44add9a337e8b76da50a39
Eliot McIntire <eliotmcintire@gmail.com>
2019-03-08 14:44:45 -0800

    Copy of data.table -- unit tests

---
+++ tests/testthat/test-Copy.R
@@ -0,0 +1,27 @@
+test_that("Copy does not work correctly", {
+  testInitOut <- testInit(smcc = FALSE, libraries = "data.table")
+  on.exit({
+    testOnExit(testInitOut)
+  }, add = TRUE)
+
+  s <- simInit()
+  s$dt <- data.table(a = 1)
+  s$dt1 <- data.table(a = 2)
+  s$dt3 <- data.table(a = 3)
+  s$dt4 <- data.frame(a = 4)
+
+  # copy -- breaks connection between data.tables
+  s1 <- Copy(s)
+  s1$dt[, a := 5]
+  expect_false(identical(s1$dt, s$dt))
+  expect_true(identical(s1$dt1, s$dt1))
+
+  # non-copy -- keeps the data.table correct
+  s2 <- s
+  s2$dt[, a := 6]
+  expect_true(identical(s2$dt, s$dt))
+  expect_true(identical(s2$dt1, s$dt1))
+
+
+
+})
