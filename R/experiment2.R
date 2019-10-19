d59887786ce0261ec4c29bd4e0ee0cc4eb1322b4
Eliot McIntire <eliotmcintire@gmail.com>
2019-10-19 13:45:09 -0700

    simLists

---
+++ R/experiment2.R
@@ -0,0 +1,110 @@
+################################################################################
+#' Run experiment, algorithm 2, using \code{\link{spades}}
+#'
+#' @inheritParams spades
+#' @param replicates The number of replicates to run of the same \code{simList}.
+#'                   See details and examples.
+#'
+#' @param clearSimEnv Logical. If TRUE, then the envir(sim) of each simList in the return list
+#'                    is emptied. This is to reduce RAM load of large return object.
+#'                    Default FALSE.
+#'
+#' @param ... Passed to \code{spades}. Specifically, \code{debug}, \code{.plotInitialTime},
+#'            \code{.saveInitialTime}, \code{cache} and/or \code{notOlderThan}. Caching
+#'            is still experimental. It is tested to work under some conditions, but not
+#'            all. See details.
+#'
+#' @inheritParams POM
+#'
+#' @details
+#' This function requires at least 1 complete simList.
+#'
+#' This function is parallel aware, using the same mechanism as used in the \code{raster}
+#' package. Specifically, if you start a cluster using \code{\link{beginCluster}}, then
+#' this experiment function will automatically use that cluster. It is always a good
+#' idea to stop the cluster when finished, using \code{\link{endCluster}}.
+#'
+#'
+#' Output directories are changed using this function: this is one of the dominant
+#' side effects of this function. If there are only replications, then a set of
+#' subdirectories will be created, one for each replicate.
+#' If there are varying parameters and or modules, \code{outputPath} is updated
+#' to include a subdirectory for each level of the experiment.
+#' These are not nested, i.e., even if there are nested factors, all subdirectories
+#' due to the experimental setup will be at the same level.
+#' Replicates will be one level below this.
+#' The subdirectory names will include the module(s), parameter names, the parameter values,
+#' and input index number (i.e., which row of the inputs data.frame).
+#' The default rule for naming is a concatenation of:
+#'
+#' 1. The experiment level (arbitrarily starting at 1). This is padded with zeros if there are
+#' many experiment levels.
+#'
+#' 2. The module, parameter name and parameter experiment level (not the parameter value,
+#' as values could be complex), for each parameter that is varying.
+#'
+#' 3. The module set.
+#'
+#' 4. The input index number
+#'
+#' 5. Individual identifiers are separated by a dash.
+#'
+#' 6. Module - Parameter - Parameter index triplets are separated by underscore.
+#'
+#' Replication is treated slightly differently. \code{outputPath} is always 1 level below the
+#' experiment level for a replicate.
+#' If the call to \code{experiment} is not a factorial experiment (i.e., it is just
+#' replication), then the
+#' default is to put the replicate subdirectories at the top level of \code{outputPath}.
+#' To force this one level down, \code{dirPrefix} can be used or a manual change to
+#' \code{outputPath} before the call to experiment.
+#'
+#' If \code{cache = TRUE} is passed, then this will pass this to \code{spades},
+#' with the additional argument \code{replicate = x}, where x is the replicate number.
+#' That means that if a user runs \code{experiment} with \code{replicate = 4} and
+#' \code{cache = TRUE}, then SpaDES will run 4 replicates, caching the results,
+#' including replicate = 1, replicate = 2, replicate = 3, and replicate = 4.
+#' Thus, if a second call to experiment with the exact same simList is passed,
+#' and \code{replicates = 6}, the first 4 will be taken from the cached copies,
+#' and replicate 5 and 6 will be run (and cached) as normal.
+#' If \code{notOlderThan} used with a time that is more recent than the cached copy,
+#' then a new spades will be done, and the cached copy will be deleted from the
+#' cache repository, so there will only ever be one copy of a particular replicate
+#' for a particular simList.
+#' NOTE: caching may not work as desired on a Windows machine because the sqlite
+#' database can only be written to one at a time, so there may be collisions.
+#'
+#' @return Invisibly returns a \code{simLists} object.
+#'
+#' @seealso \code{\link{simInit}}, \code{\link{spades}}, \code{\link{experiment}}
+#'
+#' @author Eliot McIntire
+#' @export
+#' @importFrom parallel clusterApplyLB clusterEvalQ stopCluster
+#' @importFrom raster getCluster returnCluster
+#' @rdname experiment2
+#'
+#' # @example inst/examples/example_experiment2.R
+#'
+setGeneric(
+  "experiment2",
+  signature = "...",
+  function(..., replicates = 1, clearSimEnv = FALSE,
+           cl) {
+    standardGeneric("experiment2")
+  })
+
+#' @rdname experiment
+#' @importFrom future.apply future_lapply
+setMethod(
+  "experiment2",
+  signature("simList"),
+  definition = function(..., replicates, clearSimEnv, cl) {
+    pkg <- unique(unlist(lapply(list(...), packages)))
+    outSimLists <- new("simLists")
+    ll <- list(...)
+    names(ll) <- as.character(seq_along(list(...)))
+    list2env(future_lapply(X = ll, FUN = function(sim) spades(sim),
+                         future.packages = pkg), envir = outSimLists@.xData)
+    return(outSimLists)
+  })
--- tests/testthat/test-experiment.R
+++ tests/testthat/test-experiment.R
@@ -197,3 +197,169 @@ test_that("parallel does not work with experiment function", {
     expect_gt(as.numeric(seqTime)[3], as.numeric(parTime)[3])
   }
 })
+
+test_that("experiment2 does not work correctly", {
+  skip("Not ready")
+  testInitOut <- testInit("raster", smcc = FALSE)
+  on.exit({
+    testOnExit(testInitOut)
+  }, add = TRUE)
+
+  # Example of changing parameter values
+  mySim1 <- simInit(
+    times = list(start = 0.0, end = 2.0, timeunit = "year"),
+    params = list(
+      .globals = list(stackName = "landscape", burnStats = "nPixelsBurned"),
+      # Turn off interactive plotting
+      fireSpread = list(.plotInitialTime = NA, spreadprob = c(0.2), nFires = c(10)),
+      caribouMovement = list(.plotInitialTime = NA),
+      randomLandscapes = list(.plotInitialTime = NA, .useCache = "init")
+    ),
+    modules = list("randomLandscapes", "fireSpread", "caribouMovement"),
+    paths = list(modulePath = system.file("sampleModules", package = "SpaDES.core"),
+                 outputPath = tmpdir),
+    # Save final state of landscape and caribou
+    outputs = data.frame(objectName = c("landscape", "caribou"),
+                         stringsAsFactors = FALSE)
+  )
+
+  mySim2 <- simInit(
+    times = list(start = 0.0, end = 2.0, timeunit = "year"),
+    params = list(
+      .globals = list(stackName = "landscape", burnStats = "nPixelsBurned"),
+      # Turn off interactive plotting
+      fireSpread = list(.plotInitialTime = NA, spreadprob = c(0.2), nFires = c(20)),
+      caribouMovement = list(.plotInitialTime = NA),
+      randomLandscapes = list(.plotInitialTime = NA, .useCache = "init")
+    ),
+    modules = list("randomLandscapes", "fireSpread", "caribouMovement"),
+    paths = list(modulePath = system.file("sampleModules", package = "SpaDES.core"),
+                 outputPath = tmpdir),
+    # Save final state of landscape and caribou
+    outputs = data.frame(objectName = c("landscape", "caribou"),
+                         stringsAsFactors = FALSE)
+  )
+
+  # Create an experiment - here, 2 x 2 x 2 (2 levels of 2 params in fireSpread,
+  #    and 2 levels of 1 param in caribouMovement)
+  #caribouNums <- c(100, 1000)
+  #experimentParams <- list(
+  #  caribouMovement = list(N = caribouNums)
+  #)
+
+  sims <- experiment2(mySim1, mySim2)
+  browser()
+  expt <- load(file.path(tmpdir, "experiment.RData")) %>% get() # Loads an object named experiment
+  exptDesign <- expt$expDesign
+  exptVals <- expt$expVals
+
+  expect_equal(NROW(exptDesign), 4)
+  expect_equal(exptVals[exptVals$module == "caribouMovement", "val"] %>% unlist(),
+               c(rep(caribouNums, each = 2)))
+  expect_equal(exptVals$modules %>% unique(),
+               "randomLandscapes,fireSpread,caribouMovement")
+  expect_equal(NROW(attr(sims, "experiment")$expDesign), NROW(exptDesign))
+
+  # test that experimental design object is indeed what is in the sims object
+  mods <- sapply(strsplit(names(exptDesign)[-(4:5)], split = "\\."), function(x) x[[1]])
+  params <- sapply(strsplit(names(exptDesign)[-(4:5)], split = "\\."), function(x) x[[2]])
+  out2 <- lapply(seq_along(mods), function(y) {
+    out <- lapply(seq_len(NROW(exptDesign)), function(x) {
+      expect_equivalent(0, params(sims[[x]])[[mods[y]]][[params[[y]]]] -
+                          exptVals %>% dplyr::filter(module == mods[[y]] &
+                                                       param == params[[y]] &
+                                                       expLevel == x) %>%
+                          dplyr::select(val) %>% unlist())
+    })
+  })
+
+  sims <- experiment(mySimFull, replicates = 3)
+  expt <- load(file.path(tmpdir, "experiment.RData")) %>% get() # Loads an object named experiment
+  exptDesign <- expt$expDesign
+  exptVals <- expt$expVals
+  out <- lapply(seq_along(sims), function(x) {
+    expect_equal(outputs(sims[[x]])$saved, c(TRUE, TRUE))
+    expect_equal(
+      outputs(sims[[x]])$file,
+      file.path(normPath(tmpdir), paste0("rep", x),
+                paste0(c("landscape", "caribou"), "_year2.rds")) %>%
+        normPath()
+    )
+  })
+
+  ### Test inputs - first, have to make the input map
+  mySimRL <- simInit(
+    times = list(start = 0.0, end = 0.1, timeunit = "year"),
+    params = list(
+      .globals = list(stackName = "landscape"),
+      # Turn off interactive plotting
+      randomLandscapes = list(.plotInitialTime = NA)
+    ),
+    modules = list("randomLandscapes"),
+    paths = list(modulePath = system.file("sampleModules", package = "SpaDES.core"),
+                 outputPath = file.path(tmpdir, "landscapeMaps1")),
+    outputs = data.frame(objectName = "landscape", saveTime = 0, stringsAsFactors = FALSE)
+  )
+  sims2 <- experiment(mySimRL, replicate = 2)
+
+  mySimNoRL <- simInit(
+    times = list(start = 0.0, end = 2.0, timeunit = "year"),
+    params = list(
+      .globals = list(stackName = "landscape", burnStats = "nPixelsBurned"),
+      # Turn off interactive plotting
+      fireSpread = list(.plotInitialTime = NA),
+      caribouMovement = list(.plotInitialTime = NA)
+    ),
+    modules = list("fireSpread", "caribouMovement"),
+    paths = list(modulePath = system.file("sampleModules", package = "SpaDES.core"),
+                 outputPath = tmpdir),
+    # Save final state of landscape and caribou
+    outputs = data.frame(objectName = c("landscape", "caribou"), stringsAsFactors = FALSE)
+  )
+  landscapeFiles <- dir(outputPath(mySimRL), pattern = "landscape_year0", recursive = TRUE,
+                        full.names = TRUE)
+  set.seed(1232)
+  sims <- experiment(mySimNoRL, replicates = 2,
+                     inputs = lapply(landscapeFiles, function(filenames) {
+                       data.frame(file = filenames, loadTime = 0,
+                                  objectName = "landscape", stringsAsFactors = FALSE)
+                     })
+  )
+
+  # Make sure these are using the same, identical input maps
+  expect_true(identical(sims[[1]]$landscape$habitatQuality, sims[[3]]$landscape$habitatQuality))
+  expect_true(identical(sims[[2]]$landscape$habitatQuality, sims[[4]]$landscape$habitatQuality))
+  # Make sure there are two different input maps (i.e,. the inverse of the above test)
+  expect_false(identical(sims[[2]]$landscape$habitatQuality, sims[[3]]$landscape$habitatQuality))
+
+  # Make sure random number generator is working. These start with the same maps, but should end up different
+  expect_false(identical(sims[[2]]$landscape$Fires, sims[[4]]$landscape$Fires))
+  expect_false(identical(sims[[1]]$landscape$Fires, sims[[3]]$landscape$Fires))
+
+  # Test clearSimEnv argument... i.e., clearing of the final objects
+  expect_equal(length(ls(sims[[1]])), 5)
+  set.seed(1232)
+  sims2 <- experiment(mySimNoRL, replicates = 2, clearSimEnv = TRUE,
+                      inputs = lapply(landscapeFiles, function(filenames) {
+                        data.frame(file = filenames, loadTime = 0,
+                                   objectName = "landscape", stringsAsFactors = FALSE)
+                      })
+  )
+  # This version has no objects
+  expect_equal(length(ls(sims2[[1]])), 0)
+
+  # Test that the only difference is their objects, which we can pass back in manually
+  list2env(mget(ls(sims[[1]]), envir = envir(sims[[1]])), envir = envir(sims2[[1]]))
+  expect_equal(sims[[1]], sims2[[1]])
+
+  # Test object passing in
+  experimentObj <- list(landscape = lapply(landscapeFiles, readRDS) %>%
+                          setNames(paste0("landscape", 1:2)))
+  # Pass in this list of landscape objects
+  set.seed(1232)
+  sims3 <- experiment(mySimNoRL, objects = experimentObj)
+  # Compare simulations that had objects read from disk with objects passed via objects arg
+  expect_equal(sims3[[1]]$landscape, sims[[1]]$landscape)
+  expect_equal(sims3[[2]]$landscape, sims[[2]]$landscape)
+})
+
