f040ae122fad48324556561cf11b3fc2c4a9e0cb
Eliot McIntire <eliotmcintire@gmail.com>
2019-10-20 22:53:06 -0700

    test many plans

--- tests/testthat/test-experiment2.R
+++ tests/testthat/test-experiment2.R
@@ -1,13 +1,11 @@
 test_that("experiment2 does not work correctly", {
-#  skip("Not ready")
+  if (!interactive()) skip("Not ready")
   testInitOut <- testInit(c("raster", "future.callr", "future"), smcc = FALSE)
   on.exit({
     testOnExit(testInitOut)
   }, add = TRUE)
 
 
-  # options(mc.cores = 2)
-  plan("callr", workers = 2)
   # Example of changing parameter values
   mySim1 <- simInit(
     times = list(start = 0.0, end = 2.0, timeunit = "year"),
@@ -45,46 +43,49 @@ test_that("experiment2 does not work correctly", {
                          stringsAsFactors = FALSE)
   )
 
-  # Create an experiment - here, 2 x 2 x 2 (2 levels of 2 params in fireSpread,
-  #    and 2 levels of 1 param in caribouMovement)
-  #caribouNums <- c(100, 1000)
-  #experimentParams <- list(
-  #  caribouMovement = list(N = caribouNums)
-  #)
-  # Test Caching
-  cap0 <- capture_output(mess <- capture_messages(spades(Copy(mySim1), debug = 2)))
-  expect_true(sum(grepl("cached", cap0))==0)
-  cap1 <- capture.output(mess <- capture_messages(sims <- experiment2(mySim1, mySim2)))
-  expect_true(sum(grepl("cached", cap1))==2) # b/c they are at the same time. If sequential, one would be memoised
-  cap <- capture.output(mess <- capture_messages(sims <- experiment2(mySim1, mySim2,
-                                                                     mySim1)))
-  expect_true(sum(grepl("cached", cap))==2) # these are not same session as previous, so can't memoise
-  expect_true(sum(grepl("memoised", cap))==1) # 2 were old, plus 1 was a redo in one of the workers
+  for (pl in c("sequential", "multiprocess", "callr")) {
+    cat(" -- testing future plan when", pl, "                ")
+    warn <- capture_warnings(plan(pl, workers = 2)) # just about "workers" not defined in "sequential"
+    # Test Caching
+    cap0 <- capture_output(mess <- capture_messages(spades(Copy(mySim1), debug = 2)))
+    expect_true(sum(grepl("cached", cap0))==0)
+    cap1 <- capture.output(mess <- capture_messages(sims <- experiment2(mySim1, mySim2)))
+    expects <- if (is(plan(), "sequential")) 1 else 2 # sequential has no concurrent spades
+    expect_true(sum(grepl("cached", cap1))==expects) # b/c they are at the same time. If sequential, one would be memoised
+    cap <- capture.output(mess <- capture_messages(sims <- experiment2(mySim1, mySim2,
+                                                                       mySim1)))
+    expects <- if (is(plan(), "callr")) c(2,1) else c(0,3) # uses a new session each call
+    expect_true(sum(grepl("cached", cap))==expects[1]) # these are not same session as previous, so can't memoise
+    expect_true(sum(grepl("memoised", cap))==expects[2]) # 2 were old, plus 1 was a redo in one of the workers
 
 
-  # Test replication
-  mySim1Orig <- Copy(mySim1)
-  mySim2Orig <- Copy(mySim2)
+    # Test replication
+    mySim1Orig <- Copy(mySim1)
+    mySim2Orig <- Copy(mySim2)
 
-  cap1 <- capture.output(mess <- capture_messages(
-    sims <- experiment2(mySim1, mySim2, replicates = c(2,3))
-  ))
-  # Test don't need to use Copy
-  expect_true(isTRUE(all.equal(mySim1Orig, mySim1))) # can't use identical -- envs are different
+    cap1 <- capture.output(mess <- capture_messages(
+      sims <- experiment2(mySim1, mySim2, replicates = c(2,3))
+    ))
+    # Test don't need to use Copy
+    expect_true(isTRUE(all.equal(mySim1Orig, mySim1))) # can't use identical -- envs are different
 
-  # Test replication -- can be a vector of replicates
-  expect_true(length(ls(sims)) == 5)
-  expect_true(sum(grepl("^1", sort(ls(sims)))) == 2)
-  expect_true(sum(grepl("^2", sort(ls(sims)))) == 3)
-  expect_true(sum(grepl("rep1$", sort(ls(sims)))) == 2)
-  expect_true(sum(grepl("rep2$", sort(ls(sims)))) == 2)
-  expect_false(identical(sims$`1_rep1`$caribou$x1, sims$`1_rep2`$caribou$x1))
-  expect_false(identical(sims$`1_rep1`$caribou$x1, sims$`2_rep2`$caribou$x1))
-  expect_false(identical(sims$`1_rep1`$caribou$x1, sims$`2_rep1`$caribou$x1))
-
-
-
+    # Test replication -- can be a vector of replicates
+    expect_true(length(ls(sims)) == 5)
+    expect_true(sum(grepl("^1", sort(ls(sims)))) == 2)
+    expect_true(sum(grepl("^2", sort(ls(sims)))) == 3)
+    expect_true(sum(grepl("rep1$", sort(ls(sims)))) == 2)
+    expect_true(sum(grepl("rep2$", sort(ls(sims)))) == 2)
+    expect_false(identical(sims$`1_rep1`$caribou$x1, sims$`1_rep2`$caribou$x1))
+    expect_false(identical(sims$`1_rep1`$caribou$x1, sims$`2_rep2`$caribou$x1))
+    expect_false(identical(sims$`1_rep1`$caribou$x1, sims$`2_rep1`$caribou$x1))
+  }
 })
+# # Create an experiment - here, 2 x 2 x 2 (2 levels of 2 params in fireSpread,
+# #    and 2 levels of 1 param in caribouMovement)
+# caribouNums <- c(100, 1000)
+# experimentParams <- list(
+#   caribouMovement = list(N = caribouNums)
+# )
 #
 #   expt <- load(file.path(tmpdir, "experiment.RData")) %>% get() # Loads an object named experiment
 #   exptDesign <- expt$expDesign
