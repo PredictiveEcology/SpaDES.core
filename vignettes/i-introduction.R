f7dbce970f68339bb4c85a2d47a77206e45ac219
Alex Chubaty <alex.chubaty@gmail.com>
2020-01-14 15:59:38 -0700

    redoc

--- vignettes/i-introduction.R
+++ vignettes/i-introduction.R
@@ -1,11 +1,11 @@
-## ----SpaDES-demo, eval=FALSE, echo=TRUE----------------------------------
+## ----SpaDES-demo, eval=FALSE, echo=TRUE---------------------------------------
 #  library("SpaDES.core")
 #  demo("spades-simulation", package = "SpaDES.core")
 
-## ----SpaDES-modules, eval=FALSE, echo=TRUE-------------------------------
+## ----SpaDES-modules, eval=FALSE, echo=TRUE------------------------------------
 #  downloadModule(name = "moduleName")
 
-## ----view-sim, eval=FALSE, echo=TRUE-------------------------------------
+## ----view-sim, eval=FALSE, echo=TRUE------------------------------------------
 #  # full simulation details:
 #  #  simList object info + simulation data
 #  mySim
@@ -18,13 +18,13 @@
 #  # simList object isn't shown; object names only
 #  ls(mySim)
 
-## ----view-dependencies, eval=FALSE, echo=TRUE----------------------------
+## ----view-dependencies, eval=FALSE, echo=TRUE---------------------------------
 #  library(igraph)
 #  depsEdgeList(mySim, FALSE)  # data.frame of all object dependencies
 #  moduleDiagram(mySim)        # plots simplified module (object) dependency graph
 #  objectDiagram(mySim)        # plots object dependency diagram
 
-## ----view-event-sequences, eval=FALSE, echo=TRUE-------------------------
+## ----view-event-sequences, eval=FALSE, echo=TRUE------------------------------
 #  options(spades.nCompleted = 50)   # default: store 1000 events in the completed event list
 #  mySim <- simInit(...)             # initialize a simulation using valid parameters
 #  mySim <- spades(mySim)            # run the simulation, returning the completed sim object
