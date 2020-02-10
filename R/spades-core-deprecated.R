1f24a5a4d1141f87951d1fffdc9dc12226856fcc
Eliot McIntire <eliotmci@mail.ubc.ca>
2020-02-09 23:17:34 -0800

    better deprecation of experiment fns

--- R/spades-core-deprecated.R
+++ R/spades-core-deprecated.R
@@ -1,14 +1,12 @@
-gitSite <- c("PredictiveEcology/SpaDES.experiment@development",
+gitSite1 <- c("PredictiveEcology/SpaDES.experiment@development",
              "PredictiveEcology/SpaDES.experiment@master")
-deprecatedExpt <- paste0("experiment and experiment2 are no longer in SpaDES.core. ",
-                         "Please use SpaDES.experiment::experiment or SpaDES.experiment::experiment2, ",
-                         "which for now can be done with ",
-                         "devtools::install_github('",gitSite,"')")
 
-deprecatedMsg <- function(fnName, newPackage = "SpaDES.experiment", gitSite) {
+deprecatedMsg <- function(fnName = as.character(match.call()[[1]]),
+                          newPackage = "SpaDES.experiment",
+                          gitSite = gitSite1) {
   paste0(fnName, " has been moved to ", newPackage, ". ",
          "Please install with ",
-         paste0("devtools::install_github('",gitSite, "')", collapse = " or "))
+         paste0("devtools::install_github('",gitSite1, "')", collapse = " or "))
 }
 
 #' Deprecated functions
@@ -19,28 +17,24 @@ deprecatedMsg <- function(fnName, newPackage = "SpaDES.experiment", gitSite) {
 #' @param ... Unused.
 #' @rdname deprecated
 experiment <- function(...) {
-  .Deprecated(msg = deprecatedExpt)
+  .Deprecated(msg = deprecatedMsg(match.call()[[1]]))
 }
 
 
 #' @export
 #' @rdname deprecated
 experiment2 <- function(...) {
-  .Deprecated(msg = deprecatedExpt)
+  .Deprecated(msg = deprecatedMsg(match.call()[[1]]))
 }
 
 #' @export
 #' @rdname deprecated
 POM <- function(...) {
-  fnName <- "POM"
-  .Deprecated(msg = deprecatedMsg(fnName, "SpaDES.experiment", gitSite))
+  .Deprecated(msg = deprecatedMsg(match.call()[[1]]))
 }
 
 #' @export
 #' @rdname deprecated
 simInitAndExperiment <- function(...) {
-  .Deprecated(msg = paste0("simInitAndExperiment is no longer in SpaDES.core.",
-                           "Please use SpaDES.experiment::simInitAndExperiment, ",
-                           "which for now can be done with ",
-                           "/ndevtools::install_github('",gitSite,"')"))
+  .Deprecated(msg = deprecatedMsg(match.call()[[1]]))
 }
