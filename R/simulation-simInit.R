7a41b4a3a36a2c24d96a74f4bad51bc0db15ccb6
Alex Chubaty <alex.chubaty@gmail.com>
2020-07-13 14:43:49 -0600

    cleanup

--- R/simulation-simInit.R
+++ R/simulation-simInit.R
@@ -72,24 +72,21 @@ utils::globalVariables(".")
 #'
 #' @section Parsing and Checking Code:
 #'
-#' The \code{simInit} function will attempt to find usage of sim$xxx
-#' or sim[['xxx']] on either side of the assignment "<-" operator.
-#' It will compare these to the module metadata, specifically
-#' \code{inputObjects} for cases where objects or "gotten" from the
-#' simList and \code{outputObjects} for cases where objects are
-#' assigned to the simList.
+#' The \code{simInit} function will attempt to find usage of \code{sim$xxx} or \code{sim[['xxx']]}
+#'  on either side of the assignment (\code{<-}) operator.
+#' It will compare these to the module metadata, specifically \code{inputObjects} for cases where
+#' objects or "gotten" from the \code{simList} and \code{outputObjects} for cases where objects are
+#' assigned to the \code{simList.}
 #'
-#' It will also attempt to find potential, common function name conflicts
-#' with things like scale and stack (both in base and raster), and
-#' Plot (in quickPlot and some modules).
+#' It will also attempt to find potential, common function name conflicts with things like
+#' \code{scale} and \code{stack} (both in \pkg{base} and \pkg{raster}), and
+#' \code{Plot} (in \pkg{quickPlot} and some modules).
 #'
-#' \emph{This code checking is young and may get false positives and
-#' false negatives -- i.e., miss things}. It also takes computational
-#' time, which may be undesirable in operational code.
-#' To turn off checking (i.e.,
-#' if there are too many false positives and negatives), set
-#' the option \code{spades.moduleCodeChecks} to \code{FALSE},
-#' e.g., \code{options(spades.moduleCodeChecks = FALSE)}
+#' \emph{This code checking is young and may get false positives and false negatives,
+#' i.e., miss things}.
+#' It also takes computational time, which may be undesirable in operational code.
+#' To turn off checking (i.e., if there are too many false positives and negatives), set
+#' \code{options(spades.moduleCodeChecks = FALSE)}.
 #'
 #' @section Caching:
 #'
@@ -106,7 +103,7 @@ utils::globalVariables(".")
 #' @note
 #' The user can opt to run a simpler \code{simInit} call without inputs, outputs, and times.
 #' These can be added later with the accessor methods (See example).
-#' These are not required for initializing the simulation via simInit.
+#' These are not required for initializing the simulation via \code{simInit}.
 #' All of \code{modules}, \code{paths}, \code{params}, and \code{objects} are needed
 #' for successful initialization.
 #'
@@ -116,22 +113,22 @@ utils::globalVariables(".")
 #' @param params A list of lists of the form \code{list(moduleName=list(param1=value, param2=value))}.
 #' See details.
 #'
-#' @param modules A named list of character strings specifying the names
-#' of modules to be loaded for the simulation. Note: the module name
-#' should correspond to the R source file from which the module is loaded.
-#' Example: a module named "caribou" will be sourced form the file
-#' \file{caribou.R}, located at the specified \code{modulePath(simList)} (see below).
+#' @param modules A named list of character strings specifying the names of modules to be loaded
+#' for the simulation.
+#' Note: the module name should correspond to the R source file from which the module is loaded.
+#' Example: a module named "caribou" will be sourced form the file \file{caribou.R},
+#' located at the specified \code{modulePath(simList)} (see below).
 #'
 #' @param objects (optional) A vector of object names (naming objects
 #'                that are in the calling environment of
 #'                the \code{simInit}, which is often the
-#'                \code{.GlobalEnv} unless used programmatically
-#'                -- NOTE: this mechanism will
+#'                \code{.GlobalEnv} unless used programmatically.
+#'                NOTE: this mechanism will
 #'                fail if object name is in a package dependency), or
 #'                a named list of data objects to be
-#'                passed into the simList (more reliable).
+#'                passed into the \code{simList} (more reliable).
 #'                These objects will be accessible
-#'                from the simList as a normal list, e.g,. \code{mySim$obj}.
+#'                from the \code{simList} as a normal list, e.g,. \code{mySim$obj}.
 #'
 #' @param paths  An optional named list with up to 4 named elements,
 #' \code{modulePath}, \code{inputPath}, \code{outputPath}, and \code{cachePath}.
@@ -1014,7 +1011,8 @@ simInitAndSpades <- function(times, params, modules, objects, paths, inputs, out
   #sim <- do.call(simInit, objsSimInit) # serializes the objects
 
   spadesFormals <- formalArgs(spades)[formalArgs(spades) %in% names(objsAll)]
-  objsSpades <- append(list(sim = quote(sim)), objsAll[spadesFormals]) # quote is so that entire simList is not serialized in do.call
+  ## quote is so that entire simList is not serialized in do.call
+  objsSpades <- append(list(sim = quote(sim)), objsAll[spadesFormals])
   sim <- do.call(spades, objsSpades)
 }
 
