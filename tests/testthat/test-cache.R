01d62d5152fa87900c44a98e3a72de0049cafcb5
Eliot McIntire <eliotmcintire@gmail.com>
2022-12-30 17:46:21 -0800

    trying to debug on test-coverage.yml

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -84,6 +84,7 @@ test_that("test event-level cache & memory leaks", {
       simsOut <- spades(sims, debug = FALSE)
     })
   })
+  print(warnsFunction)
   expect_true(length(grep("causing a memory leak", warnsFunction)) == 0)
 
   # Take a leaky function -- should trigger memory leak stuff
