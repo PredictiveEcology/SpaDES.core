b52311f8aef2bec0ca96731f2418d4e6802da493
Eliot McIntire <eliotmcintire@gmail.com>
2023-12-13 09:18:05 -0800

    need to update params slot from params after allowInitInSimInit

--- R/simulation-simInit.R
+++ R/simulation-simInit.R
@@ -1569,8 +1569,11 @@ resolveDepsRunInitIfPoss <- function(sim, modules, paths, params, objects, input
         objs <- mget(objsNames, objEnv)
         list2env(objs, sim$.mods[[mod]]$.objects)
       })
+      # update parameters -- from simAltOut; then from user passed params
       globals(sim) <- modifyList2(globals(sim), globals(simAltOut))
       sim <- updateParamsFromGlobals(sim)
+      sim@params <- modifyList2(sim@params, params)
+
       list2env(objs(simAltOut), envir(sim))
 
       dotUnderscoreObjs <- ls(pattern = "^._", envir(simAltOut), all.names = TRUE)
