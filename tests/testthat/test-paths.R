177a734d45083bd5426d8104403970c4c0d427ad
Alex Chubaty <achubaty@for-cast.ca>
2024-05-08 22:56:41 -0600

    fix absolutivePaths test
    with 715a13db9c50164ad6a464e5b9ab459edd783b31

--- tests/testthat/test-paths.R
+++ tests/testthat/test-paths.R
@@ -108,7 +108,7 @@ test_that("paths file does not work correctly", {
   expect_equal(terraPath(mySim), tmpdir)
 })
 
-test_that("absolutizePaths can handle multiple paths" {
+test_that("absolutizePaths can handle multiple paths", {
   test_paths <- list(
     cachePath = "/mnt/scratch/achubaty/BC_HRV/cache",
     inputPath = "mnt/projects/HRV/BC_HRV/inputs",
@@ -120,7 +120,11 @@ test_that("absolutizePaths can handle multiple paths" {
     terraPath = "/mnt/scratch/achubaty/BC_HRV/terra"
   )
 
-  absolutizePaths(paths = test_paths,
-                  projectPath = "/home/achubaty/GitHub/BC_HRV",
-                  tempdir = "/mnt/scratch/achubaty/tmp/RtmpJ0cOm1")
+  expect_no_error({
+    out <- absolutizePaths(paths = test_paths,
+                           projectPath = "/home/achubaty/GitHub/BC_HRV",
+                           tempdir = "/mnt/scratch/achubaty/tmp/RtmpJ0cOm1")
+  })
+
+  expect_equivalent(sapply(out, length), c(1, 1, 2, 1, 1, 1, 1))
 })
