4e91cec6b5e9ca27e31349faafe4c6ca9b2ed423
Alex Chubaty <achubaty@for-cast.ca>
2025-07-09 22:13:38 -0600

    update revdep/check.R

--- revdep/check.R
+++ revdep/check.R
@@ -1,4 +1,4 @@
-#remotes::install_github("r-lib/revdepcheck")
+# pak::pak("r-lib/revdepcheck")
 library("revdepcheck")
 
 options(repos = c(
@@ -9,10 +9,10 @@ options(repos = c(
   CRAN = paste0("https://", "cloud.", "r-project.", "org")
 ))
 
-revdepcheck::revdep_reset()
-revdepcheck::revdep_check(num_workers = getOption("Ncpus", 8), quiet = FALSE, timeout = 30*60) ## 30 mins
-revdepcheck::revdep_report_cran() ## update cran-comments with this output
+revdep_reset()
+revdep_check(num_workers = getOption("Ncpus", 2), quiet = FALSE, timeout = 30*60) ## 30 mins
+revdep_report_cran() ## update cran-comments with this output
 
 ### email maintainers of revdep packages (need to edit: `revdep/email.yml`)
-#revdep_email(type = "broken") ## will send via gmail
-#revdep_email(type = "failed") ## will send via gmail
+# revdep_email(type = "broken") ## will send via gmail
+# revdep_email(type = "failed") ## will send via gmail
